a90ac04abc95daf8e0528c8b778e95ffd0b70246
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:24:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,19 @@ class Triangle
   end 
   
   def triangle?
-    
+     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      false
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      false 
+    else 
+      true
   end 
   
   
   def kind 
+    if self.triangle? == false
+      
+    
     if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     end
