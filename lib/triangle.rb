51496ff44cb69a8e43d088c92684f81656a97be6
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:28:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
         :equilateral
       elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
         :isosceles
-      elsif   
+      elsif @side_1  
       end
   end 
   
