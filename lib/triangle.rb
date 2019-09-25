af6f2d7916098786498e94b8d728a20681b4a312
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:39:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,30 +8,10 @@ class Triangle
     @side_3 = side_3
   end 
   
-  def triangle?
-     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-      false
-    elsif @side_1+@side_2 <= @side_3 || @side_1+@side_3 <= @side_2 || @side_2+@side_3 <= @side_1
-      false 
-    else 
-      true
-    end
-  end 
+
   
   
-  def kind 
-    if self.triangle? == false
-      raise TriangleError
-    else
-      if @side_1 == @side_2 && @side_2 == @side_3
-        :equilateral
-      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
-        :isosceles
-      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_3 != @side_1
-        :scalene
-      end
-    end 
-  end 
+
 
 class TriangleError < StandardError
     "defined shape is not a triangle"
