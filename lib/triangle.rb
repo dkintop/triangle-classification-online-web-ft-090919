fd8b8b984e8be53770bf084794dfb18e1622703b
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:41:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
+  
+  attr_writer :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
-  end
+  end 
+  
 
-  def kind()
+    def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
@@ -23,10 +27,12 @@ class Triangle
   end
 
 end
+  
+
 
 class TriangleError < StandardError
-  # triangle error code
-end
+   
+  end
 
   
   
