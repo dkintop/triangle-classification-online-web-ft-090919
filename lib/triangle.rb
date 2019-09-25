4184f5bed0f6d9978f42c77dbed1cf1777121f75
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:37:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,9 +9,9 @@ class Triangle
   end 
   
   def triangle?
-     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
       false
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+    elsif @side_1+@side_2 <= @side_3 || @side_1+@side_3 <= @side_2 || @side_2+@side_3 <= @side_1
       false 
     else 
       true
