0a01a660b2258f29880ecb97c2ad33988342934c
Doug Kintop <dkintop@hotmail.com>
2019-09-25 16:21:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,13 +3,13 @@ class Triangle
   attr_writer :side_1, :side_2, :side_3
   
   def initialize(side_1, side_2, side_3)
-    @side_1_length = side_1
-    @side_2_length = side_2
-    @side_3_length = side_3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end 
   
   def kind 
-    if self.side_1 == self.side_2 && self.side_2 == side_3
+    if @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     end
   end 
